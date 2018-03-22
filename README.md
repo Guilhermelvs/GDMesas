5dc288d8565a24000a3e2fbf8627020351e364f1
GuilhermeLucas <guilhermeluk_s@hotmail.com>
2018-03-22 13:17:58 -0300

    Update template

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ControleMesas
+# GDMesas
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.7.3.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { MesasComponent } from './mesas/mesas.component';
 import { ControleComponent } from './controle/controle.component';
-import { MesaComponent } from './mesa/mesa.component';
-import { LocalComponent } from './local/local.component';
+import { GraficosComponent } from './graficos/graficos.component';
+import { PainelComponent } from './painel/painel.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MesasComponent,
     ControleComponent,
-    MesaComponent,
-    LocalComponent
+    GraficosComponent,
+    PainelComponent
   ],
   imports: [
     BrowserModule
